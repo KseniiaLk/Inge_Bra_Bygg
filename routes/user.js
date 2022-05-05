243fb0bd31b9781e9f4784affa8e588a3738dcdf
CodeNinjah <codeninjahblog@gmail.com>
2022-05-05 13:48:18 +0200

    taskController och routes för tasks har skrivits

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,6 @@ user.get("/", Auth.loggin, userController.auth);
 user.get("/all", Auth.admin,userController.all);
 user.get("/:id",Auth.admin,userController.getById);
 user.post("/", Auth.admin,userController.createUser);
-
 user.delete("/:id", Auth.admin,userController.delete);
 
 module.exports = user
