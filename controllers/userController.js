cc8152e14aa3c88538ba5c9cfba1b47ccca5368c
CodeNinjah <codeninjahblog@gmail.com>
2022-05-05 14:09:27 +0200

    userController är skriven

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,51 @@
-const User = require("../models/User");
+const User = require('../models/User');
 
-// module.exports = {
+class UserController{
+    async getOne(req, res, next){
+        try{
+            const user = await User.findOne({where:{id: req.params.id}})
+            if(!user){
+                //något felmeddelande
+                res.status(404).json()
+            }
+            res.json(user)
+        }catch(error){
+            next(error)
+        }
+    }
 
-//     auth: async (req, res, next) => {
-//         try{
-//             const {email, password} = req.body;
+    async getAll(req, res, next){
+        const users = await User.findAll()
+        res.json(users)
+    }
 
-//         }
-//     }
-// }
+    async create(req, res, next){
+        const { message, images } = req.body
+        const user = await User.create({message, images})
+        res.status(201).json(user)
+    }
 
-// createUser: async (req,res) => {
+    async update(req, res, next){
+        try{
+            const user = await User.findOne({where:{id: req.params.id}})
+            const { username, password_hash, role } = req.body
+            if(!user){
+                res.status(404).json()
+            }
+            user.username = username
+            user.password_hash = password_hash
+            user.role = user.role
+            user.save()
+            res.json(user)
+        }catch(error){
+            next(error)
+        }
+    }
 
-// }
-// getById: async (req, res) => {
+    async delete(req, res, next){
+        const user = await User.destroy({where: {id: req.params.id}})
+        res.status(200).json({message: 'user is deleted'})
+    }
+}
 
-// }
+module.exports = UserController
