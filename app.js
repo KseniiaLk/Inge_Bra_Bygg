22e79cd2b793fa8e082defd984fb9d94d7f43e4d
KseniiaLk <89646252+KseniiaLk@users.noreply.github.com>
2022-05-05 12:53:16 +0200

    Routes lite

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const bcrypt = require("bcryptjs");
 const express = require('express');
 const app = express();
+const routes = require ("./routes");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use("/api/user", routes.user);
+app.use("/api/tasks",routes.tasks);
 
 app.listen(3000,function(){
     console.log("Server is running..")
