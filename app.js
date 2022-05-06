a34c23ef19dc777c4f33e869ea0c1cad94a32627
CodeNinjah <codeninjahblog@gmail.com>
2022-05-06 09:25:38 +0200

    Uppdateringar

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const bcrypt = require("bcryptjs");
 const express = require('express');
-
-const { User} = require("./models/index");
-
-const app = express();
 const routes = require ("./routes");
 
-app.use(express.urlencoded({extended: true}));
+const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use("/api/user", routes.user);
 app.use("/api/tasks",routes.tasks);
 
