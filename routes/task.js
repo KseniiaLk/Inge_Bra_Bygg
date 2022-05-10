a988ff0601560e4b98f6d8ad75f2b3e4fc840da7
CodeNinjah <codeninjahblog@gmail.com>
2022-05-10 09:45:45 +0200

    Tilldelar endpoints

--- routes/task.js
+++ routes/task.js
@@ -5,6 +5,23 @@ const router = new Router()
 
 const controller = new TaskController()
 
+//Admin
+//Admin ska kunna skapa nya konton och kunna radera resurser.
+router.delete('/:id',Auth.admin, TaskController.deleteTask) //radera resurser
+
+//Worker
+//Arbetare ( worker ) ska kunna skapa ärenden kopplade till kunder, 
+//skriva meddelanden på ärenden, ladda upp en bild kopplat till ärendet och markera ärenden som klart.
+router.post('/:id',Auth.worker,TaskController.createTask) //skapa ärenden
+router.patch('/:id',Auth.worker, TaskController.updateTask) //markera task som klart
+
+//Customer
+//Kunder (client) ska kunna se sina ärenden och skriva meddelanden på sina ärenden.
+router.get('/', Auth.customer,TaskController.getTasks) //se ärenden
+router.patch('/:id',Auth.customer, TaskController.updateTask) //skriva meddelanden på ärenden
+
+//Alla endpints
+/*
 router.get('/', Auth.user,TaskController.getTasks)
 router.post('/:id',Auth.user,TaskController.createTask)
 router.patch('/:id',Auth.user, TaskController.updateTask)
@@ -13,5 +30,6 @@ router.delete('/:id',Auth.user, TaskController.deleteTask)
 router.get('/:id/message', Auth.user, TaskController.getMessages)
 router.post('/:id/message', Auth.user, TaskController.createMessage)
 router.patch('/:id/:messageId',Auth.user, TaskController.updateMessage)
+*/
 
 module.exports = router
