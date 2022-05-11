c49f79e477098710720bc4589800594a2138acca
CodeNinjah <codeninjahblog@gmail.com>
2022-05-11 10:42:23 +0200

    Login works now

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const { User } = require("../models");
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 require('dotenv').config({path: './config/.env'});
@@ -6,20 +6,20 @@ require('dotenv').config({path: './config/.env'});
 module.exports = {
   async authenticate(req, res) {
     try{
-    //const user = await User.authenticate(req.body.username, req.body.password_hash);
-    const payload ={
-      name: req.body.username,
-      password: req.body.password_hash
-    }
+    const user = await User.findOne({where: {username: req.body.username}})   
 
-    const user = await User.findOne({where: {username: payload.name}})
-    
-    if(user && (await bcrypt.compare(payload.password, user.password_hash))){
+    if(user && (await bcrypt.compare(req.body.password, user.password_hash))){
+
+      const payload ={
+        id: user.user_id,
+        name: req.body.username,
+      }
+
       const token = jwt.sign(payload, process.env.JWT_SECRET_TOKEN)
       //skicka token till db och sedan tillbaka!
       user.token = token
       //res.json({token, payload})
-      res.status(200).json(user)
+      res.status(200).json({user, token})
     }
     res.status(400).send("Invalid Credentials")
   } catch(err){
