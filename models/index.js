c73052d1feea0ac502b8e2d1f86de0038bf075bb
CodeNinjah <codeninjahblog@gmail.com>
2022-05-11 09:50:57 +0200

    Working with the Task model and its relations

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize')
 //const User = require('./User')
 const setupUser = require('./User')
 const setupTask = require('./Task')
+const setupMessage = require('./Message')
 
 // Glöm inte sätta relation
 const sequelize = new Sequelize({
@@ -11,13 +12,17 @@ const sequelize = new Sequelize({
 
 const User = setupUser(sequelize)
 const Task = setupTask(sequelize)
+const Message = setupMessage(sequelize)
 
 User.hasMany( Task, {foreignKey: {name:'worker_id', allowNull:false}})
 Task.belongsTo( User, {
     foreignKey: 'worker_id'
 }) //med en foregin key
 
-User.hasMany( Task, {foreignKey: 'client_id'})
+User.hasMany( Task, {foreignKey: 'client_id'} )
 Task.belongsTo( User, {foreignKey: 'client_id'} )
+
+Task.hasMany( Message, {foreignKey: 'task_id'})
+Message.belongsTo( Task, {foreignKey: 'task_id'})
 
 module.exports = {User, Task, sequelize}
